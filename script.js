3637cd0119cfbca2f968927cf71fa3fb5263ec39
Ricardo Guerrero <ricardogro@outlook.com>
2026-04-28 13:57:26 -0700

    Implement drawing with hover action and select color mode

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const MAX_GRID_SIZE = 100;
 
 const grid = document.getElementById("grid");
 const sizeInput = document.getElementById("grid-size");
+const colorModeSelect = document.getElementById("color-mode");
 const setGridButton = document.getElementById("set-grid-size");
 
 function normalizeGridSize(size) {
@@ -20,8 +21,27 @@ function createGrid(size) {
         cell.classList.add("cell");
         cell.style.width = `${cellSize}px`;
         cell.style.height = `${cellSize}px`;
+        cell.addEventListener("mouseenter", () => {
+            cell.style.backgroundColor = getDrawColor();
+        });
         grid.appendChild(cell);
     }
+}
+
+function getDrawColor() {
+    if (colorModeSelect.value === "rainbow") {
+        return getRandomRgbColor();
+    }
+
+    return "#000";
+}
+
+function getRandomRgbColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
 }
 
 setGridButton.addEventListener("click", () => {
